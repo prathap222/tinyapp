5ca0d44f7a2ec82bb7c229ed46e6fdebd9664423
Prathap <prathap.sims@gmail.com>
2022-07-12 05:30:07 +0000

    adding second route

--- express_server.js
+++ express_server.js
@@ -17,24 +17,26 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+// app.get("/hello", (req, res) => {
+//   res.send("<html><body>Hello <b>World</b></body></html>\n");
+// });
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
 app.get("/hello", (req, res) => {
-  // res.send("<html><body>Hello <b>World</b></body></html>\n");
-  res.send("curl -i http://localhost:8080/hello");
+  const templateVars = { greeting: "Hello World!" };
+  res.render("hello_world", templateVars);
 });
 
-app.get("/set", (req, res) => {
-  const a = 1;
-  res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
+app.get("/urls/:shortURL", (req, res) => {
+  let shortURL = req.params.shortURL;
+  let longURL = urlDatabase[req.params.shortURL];
+  let templateVars = { shortURL: shortURL, longURL: longURL };
+  res.render("urls_show", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
