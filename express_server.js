474e0f4cb9512eb54168db0ef007bb840219c8c8
Prathap <prathap.sims@gmail.com>
2022-07-12 23:49:58 +0000

    added delete opreation

--- express_server.js
+++ express_server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -17,27 +18,86 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
-
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+// disply all urls to main page
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-
-app.get("/hello", (req, res) => {
-  const templateVars = { greeting: "Hello World!" };
-  res.render("hello_world", templateVars);
+// new url is created
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
+// new page 
 app.get("/urls/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
-  let longURL = urlDatabase[req.params.shortURL];
-  let templateVars = { shortURL: shortURL, longURL: longURL };
-  res.render("urls_show", templateVars);
+  if (verifyShortUrl(shortURL)) {
+    let longURL = urlDatabase[req.params.shortURL];
+    let templateVars = { shortURL: shortURL, longURL: longURL };
+    res.render("urls_show", templateVars);
+  } else {
+    res.send('does not exist');
+  }
+});
+
+//new url added to shown with all urls
+
+app.post("/urls", (req, res) => {
+  const shortURL = generateShortURL();
+  const newURL = req.body.longURL;
+  urlDatabase[shortURL] = newURL;
+  res.redirect(`/urls/${shortURL}`);         
+});
+// redirect to longRIL
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  if (verifyShortUrl(shortURL)) {
+    const longURL = urlDatabase[shortURL];
+    res.redirect(longURL);
+  } else {
+    res.status(404);
+    res.send('Does not exist');
+  }
+});
+//post route that removes a url resource: POST
+app.post("/urls/:shortURL/delete",(req,res) => {
+  const urlTodelete = req.params.shortURL;
+  delete urlDatabase[urlTodelete];
+  res.redirect('/urls');
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+const generateRandomString = () => {
+  const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+  const upperCase = lowerCase.toUpperCase();
+  const numeric = '1234567890';
+  const alphaNumeric = lowerCase + upperCase + numeric;
+  //alphaNumeric is 62
+  let index = Math.round(Math.random() * 100);
+  if (index > 61) {
+    while (index > 61) {
+      index = Math.round(Math.random() * 100);
+    }
+  }
+  return alphaNumeric[index];
+};
+
+//generate a unique url, string random alphaNumeric values
+const generateShortURL = () => {
+  let randomString = '';
+  while (randomString.length < 6) {
+    randomString += generateRandomString();
+  }
+  return randomString; };
+
+
+//this will show if short url exists
+const verifyShortUrl = URL => {
+  return urlDatabase[URL];
+};
